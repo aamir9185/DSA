fd1b354e06bbbf9354219a6a6e3d038faaf72b96
aamir9185 <mohammadaamir9185@gmail.com>
2025-02-05 21:23:07 +0530

    5th Feb

--- 54.java
+++ 54.java
@@ -1,7 +1,49 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        
+        List<Integer> result = new ArrayList<>();
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int up = 0;
+        int down = rows -1;
+        int left = 0;
+        int right = cols -1;
+        int totalElements = rows * cols ;
+
+        int count = 0 ; 
+        while (count < totalElements) {
+
+            //left to right
+             for (int col = left; col <= right && count < totalElements;col++) {
+                result.add(matrix[up][col]);
+                count ++;
+             }
+            //top to bottom
+            for (int row = up + 1; row <= down && count < totalElements; row++) {
+                result.add(matrix[row][right]);
+                count ++;
+            }
+            //right to left
+            for (int col = right - 1; col >= left && count < totalElements; col--) {
+                result.add(matrix[down][col]);
+                count ++;
+            }
+
+            //bottom to up
+            for (int row = down -1; row >  up && count < totalElements; row--) {
+                result.add(matrix[row][left]);
+                count ++;
+            }
+            up ++;
+            down --;
+            right --;
+            left ++;
+
+        }
+
+        return result;
     }
 }
